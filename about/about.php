f2be9f93e33b92129594f4ff7d19f83ddca7af12
jayandisemini2003-pixel <jayandisemini2003@gmail.com>
2026-07-21 00:05:37 +0530

    fix(about): add missing style.css link to fix layout

--- about/about.php
+++ about/about.php
@@ -30,6 +30,7 @@ $timeline_images = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Debuglia - About Us</title>
+    <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="about.css">
     <link rel="stylesheet" href="../ui-polish.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
